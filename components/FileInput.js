633f588500b84000f8e713c9cc0d2f72030e0e82
lawynnjana <jlawynn@gmail.com>
2020-06-24 15:24:23 -0400

    update file input style

--- components/FileInput.js
+++ components/FileInput.js
@@ -1,11 +1,8 @@
 const FileInput = ({ onChange, className }) => {
   return (
     <div className={`input-group ${className}`}>
-      <div className="custom-file">
-        <input id="file-upload" type="file" onChange={onChange} />
-        <label className="custom-file-label" htmlFor="file-upload">
-          Choose file
-        </label>
+      <div class="form-group">
+        <input type="file" class="form-control-file" onChange={onChange} />
       </div>
       <style jsx>{`
         .custom-file-upload {
