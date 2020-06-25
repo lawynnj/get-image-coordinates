26fefbc88c2bdbecc7123249e9c2e20a4adee2da
lawynnjana <jlawynn@gmail.com>
2020-06-24 22:25:33 -0400

    delete fileinput component

--- components/FileInput.js
+++
@@ -1,9 +0,0 @@
-const FileInput = ({ onChange, className = "" }) => {
-  return (
-    <div className={`form-group ${className}`}>
-      <input type="file" className="form-control-file" onChange={onChange} />
-    </div>
-  );
-};
-
-export default FileInput;
