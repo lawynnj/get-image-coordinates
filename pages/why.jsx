9de82300c38bc07a433fd8fd8dd25bb8f2ef7734
lawynnjana <jlawynn@gmail.com>
2021-03-16 16:09:26 -0400

    fix why page styles

--- pages/why.jsx
+++ pages/why.jsx
@@ -5,7 +5,7 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 export default () => {
   return (
     <Layout title="Why? - The motivation">
-      <Jumbotron>
+      <Jumbotron style={{ height: "100vh", marginBottom: 0 }}>
         <h2>The Motivation</h2>
         <p>
           We had a task that required us to find over 10 pairs of x and y
@@ -21,12 +21,10 @@ export default () => {
         </p>
         <h2>Solution</h2>
         <p>
-          To make this tedious process more efficient, we decided to create an
-          app that allows us to upload an image and click on a point in the
-          image to copy the coordinates in a custom format. In our case it was
-          JSON format.
+          To make this tedious process more efficient, I created an app that
+          allows us to upload an image and click on a point in the image to copy
+          the coordinates in a custom format. In our case it was JSON format.
         </p>
-        <p>In construction: SHOW GIF</p>
       </Jumbotron>
     </Layout>
   );
